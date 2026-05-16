6fcef6f9225f4960454503fc1307b5d84c643d4e
Fedee-Goncalvess <federico.gon.923@gmail.com>
2026-05-16 16:11:06 -0300

    LCD header y source

--- Microchip/TP2-Grupo12/lcd_microwave.c
+++ Microchip/TP2-Grupo12/lcd_microwave.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "lcd_microwave.h"
 #include <stdio.h>
 #include <string.h>
@@ -27,3 +28,66 @@ const char* lcd_get_content(void)
 {
 	return lcd_buffer;
 }
+=======
+/*=====[Inclusion of own header]====================================*/
+#include "lcd_microwave.h"
+
+/*=====[Inclusions of private function dependencies]================*/
+#include "lcd.h"
+
+/*=====[Definition macros of private constants]=====================*/
+
+
+/*=====[Private function-like macros]===============================*/
+//#define moduleConfig moduleInit
+
+/*=====[Definitions of private data types]=========================*/
+// Function pointer data type
+//typedef void (*FuncPtrPrivado_t)(void *);
+
+/*=====[Definitions of external public global variables]============*/
+//extern int32_t varGlobalExterna;
+
+/*=====[Definitions of public global variables]=====================*/
+//int32_t varGlobalPublica = 0;
+
+/*=====[Definitions of private global variables]====================*/
+
+
+/*=====[Prototypes (declarations) of private functions]=============*/
+
+
+/*=====[Implementations of public functions]========================*/
+void LCD_Setup_Reposo(void){
+	LCDclr();
+	LCDGotoXY(0,0);
+	LCDescribeDato(00,2);
+	LCDsendChar(':');
+	LCDescribeDato(00,2);
+}
+
+void LCD_Reposo_Refresh(uint8_t num){
+	static uint8_t i=4;
+	
+	LCDGotoXY(i,0);
+	LCDescribeDato(num,1);
+
+	if (i==0){
+		i=4;
+	}else {
+		i--;
+		if (i==2) i--;	
+	}
+}
+
+void LCD_Setup_Begin(){
+	LCD_Init();
+}
+
+/*=====[Implementations of interrupt functions]=====================*/
+//void UART0_IRQHandler(void) {
+// ...
+//}
+
+/*=====[Implementations of private functions]=======================*/
+>>>>>>> 21ddb75 (LCD header y source)
--- Microchip/TP2-Grupo12/lcd_microwave.h
+++ Microchip/TP2-Grupo12/lcd_microwave.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef _LCD_H_
 #define _LCD_H_
 
@@ -9,3 +10,50 @@ void lcd_write_string(const char *str);
 const char* lcd_get_content(void);
 
 #endif
+=======
+/***********************************************************************
+* Archivo: lcd_microwave.h
+* Descripción:
+*   Implementación del LCD del horno microondas.
+*
+***********************************************************************/
+
+/*=====[Avoid multiple inclusion - begin]===========================*/
+#ifndef _LCD_MICROWAVE_H_
+#define _LCD_MICROWAVE_H_
+
+/*=====[Inclusions of public function dependencies]=================*/
+
+
+/*=====[Definition macros of public constants]======================*/
+
+/*=====[Prototypes (declarations) of public functions]==============*/
+
+
+/**
+ * @brief   Actualiza tiempo de microondas desde segundos a minutos.
+ *
+ * @param	n Numero válido de 0-9 para agregar al tiempo
+ *
+ * @note    Muestra nn:nn en pantalla.
+ *          Debe llamarse al presionarse una tecla del keypad.
+ */
+void LCD_Reposo_Refresh(uint8_t n);
+/**
+ * @brief   Inicializa LCD para el estado de reposo.
+ *
+ * @note    Muestra 00:00 en pantalla.
+ *          Debe llamarse al pasar al estado de reposo.
+ */
+void LCD_Setup_Reposo(void);
+
+/**
+ * @brief   Inicializa LCD.
+ *
+ * @note    Debe llamarse antes de utilizar el LCD.
+ */
+void LCD_Setup_Begin(void);
+
+/*=====[Avoid multiple inclusion - end]=============================*/
+#endif /* _LCD_MICROWAVE_H_ */
+>>>>>>> 21ddb75 (LCD header y source)
