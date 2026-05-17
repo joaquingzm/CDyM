ab983e67f844d63201d29c5fa9621e3dd9fc5e27
Fedee-Goncalvess <federico.gon.923@gmail.com>
2026-05-16 22:49:58 -0300

    Ultimo commit de la night

--- Microchip/TP2-Grupo12/main.c
+++ Microchip/TP2-Grupo12/main.c
@@ -3,11 +3,13 @@
 #include "microwave.h"
 #include "lcd_microwave.h"
 #include <util/delay.h>
-//#include "timer.h"
+#include "timer.h"
 
 int main(void)
-{	/*
+{
 	timer_init();
+	KEYPAD_init();
+	LCD_Setup_Begin();
     microwave_init();
     while (1) 
     {
@@ -15,30 +17,10 @@ int main(void)
 			microwave_update();
 			FLAG_TIMER = 0;
 		}
-    }
-	*/
-	 KEYPAD_init();
-	 LCD_Setup_Begin();
-	 microwave_init();
-/*
-	 keypad_simulate_key('1');
-	 microwave_update();
-
-	 keypad_simulate_key('3');
-	 microwave_update();
-
-	 keypad_simulate_key('0');
-	 microwave_update();
-	 
-	 keypad_simulate_key('0');
-	 microwave_update();
-
-	 keypad_simulate_key('A');
-	 microwave_update();
-*/
-
-	 while(1) {
+		
+	/*	
 		microwave_update();
-		_delay_ms(10);     // imita el tick del timer a 100ms
-	 }
+		_delay_ms(5);
+		*/
+    }
 }
