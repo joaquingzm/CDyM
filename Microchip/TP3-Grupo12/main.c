113ca3cb22dc78fe16fbf8ddbd072e4a2a74fe74
Joaquin <joaquin.gzm234@gmail.com>
2026-06-23 17:14:32 -0300

    Arrancando tp3

---
+++ Microchip/TP3-Grupo12/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP3-Grupo12.c
+ *
+ * Created: 6/23/2026 14:48:07
+ * Author : joaqu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
