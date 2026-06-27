81da543f3ff70244061d2e1ee59befe35581e618
Fedee-Goncalvess <federico.gon.923@gmail.com>
2026-06-27 02:09:19 -0300

    EL SENSOR FUNCIONA

--- Microchip/TP3-Grupo12/driver_dht11.c
+++ Microchip/TP3-Grupo12/driver_dht11.c
@@ -1,23 +1,24 @@
 /*===== [Inclusion of own header] ===============================*/
 #include "driver_dht11.h"
-
+#define F_CPU 16000000UL
 /*===== [Inclusions of private function dependencies] ==========*/
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 /*===== [Definition macros of private constants] ================*/
 #define DHT_PORT        PORTC
 #define DHT_DDR         DDRC
 #define DHT_PINR        PINC
-#define DHT_BIT         PC0
+#define DHT_BIT         PORTC0
 
 #define DHT_START_MS    18
-#define DHT_WAIT_US     100
+#define DHT_WAIT_US     200//140
 #define DHT_BIT_DELAY   30
 
 /*===== [Private function-like macros] ==========================*/
 #define DHT_SET_OUTPUT()    (DHT_DDR |=  (1 << DHT_BIT))
-#define DHT_SET_INPUT()     (DHT_DDR &= ~(1 << DHT_BIT))
+#define DHT_SET_INPUT() (DHT_DDR &= ~(1 << DHT_BIT))
 #define DHT_LOW()           (DHT_PORT &= ~(1 << DHT_BIT))
 #define DHT_HIGH()          (DHT_PORT |=  (1 << DHT_BIT))
 #define DHT_READ()          (DHT_PINR &   (1 << DHT_BIT)) 
@@ -26,94 +27,110 @@
 static uint8_t dht_raw[5]; //40bit de respuesta del sensor
 
 /*===== [Prototypes (declarations) of private functions] =======*/
+static inline void timer1_start(void);
+static void timer1_stop(void);
+static dht11_status_t dht_check_presence(void);
+static dht11_status_t dht_read_raw(void);
+
 
 /*===== [Implementations of public functions] ==================*/
 dht11_status_t dht11_read(dht11_data_t *data)
 {
 	dht11_status_t status;
-
-	status = dht_check_presence();	//Veerificamos el estado del sensor
-	if (status != DHT11_OK)
-	return status;
-
+	cli();
+	status = dht_check_presence();	//Verificamos el estado del sensor
+	if (status != DHT11_OK){
+		sei();	
+		return status;	
+	}
+	
 	status = dht_read_raw();
-	timer0_stop();
-	if (status != DHT11_OK)
-	return status;
+	timer1_stop();
+	if (status != DHT11_OK){
+		sei();
+		return status;
+	}
 
 	/* checksum */ //Verifica que la suma de los datos enviados mod 256 sea igual al que tiene el sensor
-	if ((uint8_t)(dht_raw[0] + dht_raw[1] + dht_raw[2] + dht_raw[3]) != dht_raw[4])
-	return DHT11_ERR_CHECKSUM;
+	if ((uint8_t)(dht_raw[0] + dht_raw[1] + dht_raw[2] + dht_raw[3]) != dht_raw[4]){
+		sei();
+		return DHT11_ERR_CHECKSUM;	
+	}
 
 	data->humidity_int    = dht_raw[0];
 	data->humidity_dec    = dht_raw[1];
 	data->temperature_int = dht_raw[2];
 	data->temperature_dec = dht_raw[3];
 
+	sei();
 	return DHT11_OK;
 }
 
 /*===== [Implementations of private functions] ==================*/
-static inline void timer0_start(void)
+static inline void timer1_start(void)
 {
-	TCNT0 = 0;
-	TCCR0B |= (1 << CS00);   // prescaler = 1 (ejemplo)
+	TCNT1 = 0;
+	TCCR1A = 0;
+	TCCR1B = (1 << CS11); // prescaler = 8
 }
 
-static inline void timer0_stop(void)
+static inline void timer1_stop(void)
 {
-	TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00));
+	TCCR1B = 0;
 }
 
 
-static dht11_status_t dht_wait_level(uint8_t level, uint16_t timeout_us)
+static dht11_status_t dht_wait_level(uint8_t level, uint16_t timeout_ticks)
 {
-	TCNT0 = 0; //set contador timer0 en 0
-
-	while (((DHT_READ() != 0) != level)) {
-		if (TCNT0 >= timeout_us) {
-			return DHT11_ERR_TIMEOUT; //Si el timer se pasa del límite de timeout, el sensor no respondió
+	timer1_start();
+	if (level) {
+		while (!(PINC & (1 << PORTC0))) {
+			if (TCNT1 >= timeout_ticks){ 
+				timer1_stop(); 
+				return DHT11_ERR_TIMEOUT;
+			};
+		}
+		} else {
+		while (PINC & (1 << PORTC0)) {
+			if (TCNT1 >= timeout_ticks){
+				timer1_stop();
+				return DHT11_ERR_TIMEOUT;
+			};
 		}
 	}
-
 	return DHT11_OK;
 }
 
 static dht11_status_t dht_check_presence(void)
 {
-	/* START */
+
+	//START
 	DHT_SET_OUTPUT();	//Enviamos 18ms de low 
 	DHT_LOW();
 	_delay_ms(DHT_START_MS);
-	DHT_HIGH();			//Luego 30us de high para que sensor responda
-	_delay_us(30);
 	DHT_SET_INPUT();	//Volvemos a setear pin como input
+	_delay_us(30);
 
-	timer0_start();
+	 //Sensor response 
+	if (dht_wait_level(0, 60) != DHT11_OK) {
+		return DHT11_ERR_CHECKSUM;
+	}
 
-	/* Sensor response */
-	if (dht_wait_level(0, 100) != DHT11_OK) {
-		timer0_stop();
+	if (dht_wait_level(1, 160) != DHT11_OK) {
 		return DHT11_ERR_NO_RESPONSE;
 	}
 
-	if (dht_wait_level(1, 100) != DHT11_OK) {
-		timer0_stop();
+	if (dht_wait_level(0, 160) != DHT11_OK) {
 		return DHT11_ERR_NO_RESPONSE;
 	}
-
-	if (dht_wait_level(0, 100) != DHT11_OK) {
-		timer0_stop();
-		return DHT11_ERR_NO_RESPONSE;
-	}
-
 	return DHT11_OK;
+	
 }
 
 static dht11_status_t dht_read_raw(void)
 {
 	uint8_t i, j;
-
+	uint8_t aux=0;
 	for (i = 0; i < 5; i++) {
 		dht_raw[i] = 0;
 	}
@@ -121,19 +138,19 @@ static dht11_status_t dht_read_raw(void)
 	for (i = 0; i < 5; i++) {
 		for (j = 0; j < 8; j++) {
 
-			/* esperar inicio del bit (HIGH) */
-			if (dht_wait_level(1, 70) != DHT11_OK)
-			return DHT11_ERR_TIMEOUT;
+			// esperar inicio del bit (HIGH) 
+			
+			
+			if (dht_wait_level(1, 160) != DHT11_OK){
+				aux= TCNT1;
+				return DHT11_ERR_TIMEOUT;
+			}
+			// esperar fin del HIGH 
+			if (dht_wait_level(0, 240) != DHT11_OK) return DHT11_ERR_TIMEOUT;
 
-			TCNT0 = 0;
+			uint8_t pulse_width = aux+TCNT1;
 
-			/* esperar fin del HIGH */
-			if (dht_wait_level(0, 100) != DHT11_OK)
-			return DHT11_ERR_TIMEOUT;
-
-			uint8_t pulse_width = TCNT0;
-
-			if (pulse_width > 40) {   // ~70 µs ? bit 1
+			if (pulse_width > 70) {   // ~70 µs ? bit 1
 				dht_raw[i] |= (1 << (7 - j));
 			}
 		}
--- Microchip/TP3-Grupo12/main.c
+++ Microchip/TP3-Grupo12/main.c
@@ -7,6 +7,8 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <string.h>
+//añado pruebas del sensor
+#include "driver_dht11.h"
 
 int main(void)
 {
@@ -20,15 +22,47 @@ int main(void)
 	while(1)
 	{
 		terminal_dispatch();
-		
+		//msg[0]='a';
 		if(FLAG_TIMER)
 		{
 			FLAG_TIMER = 0;
+			//msg[0]=msg[0]+1;
 			
 			// TELEMETRIA
 			
 			// ------ ARMO MSG -------
+			dht11_data_t data_dht11;
+			dht11_status_t status_dht11;
+			status_dht11 = dht11_read(&data_dht11);
+			if (status_dht11 == DHT11_ERR_NO_RESPONSE){
+				strcpy(msg, "ERROR DE SENSOR: NO RESPONSE");
+			}
 			
+			if (status_dht11 == DHT11_ERR_CHECKSUM){
+				strcpy(msg, "ERROR DE SENSOR: CHECKSUM INCORRECTO");
+			}
+			
+			if (status_dht11 == DHT11_ERR_TIMEOUT){
+				strcpy(msg, "ERROR DE SENSOR: TIMEOUT");
+			}
+			
+			if(status_dht11 == DHT11_OK){
+				char t1,t2,h1,h2;
+				t1 = (data_dht11.temperature_int / 10) + '0'; //decena
+				t2 = (data_dht11.temperature_int % 10) + '0'; //unidad 
+				h1 = (data_dht11.humidity_int / 10) + '0';
+				h2 = (data_dht11.humidity_int % 10) + '0';
+				
+				msg[0] = 'T';
+				msg[1] = '=';
+				msg[2] = t1;
+				msg[3] = t2;
+				msg[4] = '|';
+				msg[5] = 'H';
+				msg[6] = '=';
+				msg[7] = h1;
+				msg[8] = h2;
+			}
 			// ------ FIN ARMADO MSG -----
 			
 			// ------ MENSAJE ------
