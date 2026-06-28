79ac69ac5882a083fffb17907e3210885f4691d9
Fedee-Goncalvess <federico.gon.923@gmail.com>
2026-06-27 23:59:36 -0300

    cositas

--- Microchip/TP3-Grupo12/main.c
+++ Microchip/TP3-Grupo12/main.c
@@ -1,20 +1,19 @@
 #define F_CPU 16000000UL
 
 #include <avr/io.h>
-#include "driver_usart0.h"
-#include "terminal.h"
-#include "timer.h"
+//#include "driver_usart0.h"
+//#include "terminal.h"
+//#include "timer.h"
 #include <avr/interrupt.h>
-#include <util/delay.h>
-#include <string.h>
+//#include <util/delay.h>
+//#include <string.h>
 //añado pruebas del sensor
-#include "driver_dht11.h"
+//#include "driver_dht11.h"
 #include "monitor.h"
 
 int main(void)
 {
 	monitor_init();
-	timer_init();
 	sei();
 
 	while(1)
